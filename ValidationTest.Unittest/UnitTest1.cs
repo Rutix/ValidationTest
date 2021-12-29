793150ef0f1f62c4832f86cb17e8eb912db5e9f4
Rutger Storm <r.storm@omniplan.nl>
2021-12-29 15:53:12 +0100

    Add unittest

---
+++ ValidationTest.Unittest/UnitTest1.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace ValidationTest.Unittest
+{
+	public class UnitTest1
+	{
+		[Fact]
+		public void Test1()
+		{
+			var sut = new Foobar()
+			{
+				Bar = "Bar",
+				Foo = "Foo"
+			};
+
+			var errors = Validate(sut);
+			Assert.NotEmpty(errors);
+		}
+
+		public List<ValidationResult> Validate(object sut)
+		{
+			var ctx = GetValidationContext(sut);
+			var errors = new List<ValidationResult>();
+			Validator.TryValidateObject(sut, ctx, errors, true);
+			return errors;
+		}
+
+		public ValidationContext GetValidationContext(object input)
+		{
+			IServiceCollection serviceCollection = new ServiceCollection();
+			var serviceProvider = serviceCollection.BuildServiceProvider();
+			var context = new ValidationContext(input, serviceProvider, null);
+			return context;
+		}
+	}
+}
